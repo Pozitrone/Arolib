6c283cce56662da249f18c010626b660c0351587
Sretr <jiri.sretr@microton.cz>
2019-08-12 21:02:58 +0200

    Added error handling.

--- AroLib.lua
+++ AroLib.lua
@@ -38,83 +38,111 @@ end
 
 
 function arolib.tps() -- TPS function by Nex4rius
+    if not pcall(
+        function()
+            local function time()
+                local f = io.open("/tmp/TPS","w")
+                f:write("Woof!")
+                f:close()
+                return(require("filesystem").lastModified("/tmp/TPS"))
+            end
 
-    local function time()
-        local f = io.open("/tmp/TPS","w")
-        f:write("Woof!")
-        f:close()
-        return(require("filesystem").lastModified("/tmp/TPS"))
-    end
-
-    local realTimeOld = time()
-    os.sleep(1)
-    ticks = 20000 / (time() - realTimeOld)
-    if ticks > 20 then
-        return 20
-    else
-        return ticks
+            local realTimeOld = time()
+            os.sleep(1)
+            ticks = 20000 / (time() - realTimeOld)
+            if ticks > 20 then
+                return 20
+            else
+                return ticks
+            end
+        end
+    ) then
+        print("An Error ocurred. Your harddrive might not have enough capacity.")
     end
 end
 
 
 function arolib.farmsControl() -- Outputs redstone from the back, when tps > 15, else stops the signal. Checks every 10 seconds.
-    while true do
-        local tps = tps()
-        if tps > 15 then
-            print("TPS status nominal, farms running.")
-            colortps(tps)
-            require("component").redstone.setOutput(sides.back, 15)
-        else
-            print("TPS are too low, farm are not running!")
-            colortps(tps)
-            require("component").redstone.setOutput(sides.back, 0)
+    if not pcall(
+        function()
+            while true do
+                local tps = tps()
+                if tps > 15 then
+                    print("TPS status nominal, farms running.")
+                    colortps(tps)
+                    require("component").redstone.setOutput(sides.back, 15)
+                else
+                    print("TPS are too low, farm are not running!")
+                    colortps(tps)
+                    require("component").redstone.setOutput(sides.back, 0)
+                end
+                os.sleep(10)
+            end
         end
-        os.sleep(10)
+    ) then
+        print("An Error occured. Check, if a redstone card is present.")
     end
 end
 
 
 function arolib.colortps() -- Prints out TPS in according color
-    tps = arolib.tps()
-    if tps > 15 then
-        require("component").gpu.setForeground(clrGreen)
-    elseif tps > 5 then
-        require("component").gpu.setForeground(clrOrange)
-    else
-        require("component").gpu.setForeground(clrRed)
+    if not pcall(
+        function()
+            tps = arolib.tps()
+            if tps > 15 then
+                require("component").gpu.setForeground(clrGreen)
+            elseif tps > 5 then
+                require("component").gpu.setForeground(clrOrange)
+            else
+                require("component").gpu.setForeground(clrRed)
+            end
+            print(tps)
+            require("component").gpu.setForeground(clrWhite)
+        end
+    ) then
+        print("An error occured.")
     end
-    print(tps)
-    require("component").gpu.setForeground(clrWhite)
 end
 
 
 function arolib.draconicCore()
-    local core = require("component").draconic_rf_storage
-    print("use arolib.reset() to reset the Resolution back.");
-    os.sleep(4)
-    require("component").gpu.setResolution(40,7);
-    require("term").clear()
-    while true do
-        print("Energy stored: ")
-        print(core.getEnergyStored())
-        print("Maximum energy: ")
-        print(core.getMaxEnergyStored())
-        print("Transfering: ")
-        local rate = core.getTransferPerTick()
-        if rate > 0 then 
-            require("component").gpu.setForeground(clrGreen)
-        else
-            require("component").gpu.setForeground(clrRed)
+    if not pcall(
+        function()
+            local core = require("component").draconic_rf_storage
+            print("use arolib.reset() to reset the Resolution back.");
+            os.sleep(4)
+            require("component").gpu.setResolution(40,7);
+            require("term").clear()
+            while true do
+                print("Energy stored: ")
+                print(core.getEnergyStored())
+                print("Maximum energy: ")
+                print(core.getMaxEnergyStored())
+                print("Transfering: ")
+                local rate = core.getTransferPerTick()
+                if rate > 0 then 
+                    require("component").gpu.setForeground(clrGreen)
+                else
+                    require("component").gpu.setForeground(clrRed)
+                end
+                print(rate)
+                require("component").gpu.setForeground(clrWhite)
+                os.sleep(1)
+            end
         end
-        print(rate)
-        require("component").gpu.setForeground(clrWhite)
-        os.sleep(1)
-    end
+    ) then
+        print("An error occured. Please, make sure a draconic energy storage core is connected.")
 end
 
 function arolib.reset()
-    require("component").gpu.setResolution(160,50);
-    require("term").clear()
+    if not pcall(
+        function()
+            require("component").gpu.setResolution(160,50);
+            require("term").clear()
+        end
+    ) then
+        print("An error occured.")
+    end
 end
 
 return arolib
